5bdad6c4796cbce698f738a093de2bc9da6cb5b1
Abdullrahman Hawamdeh <abdullrahmanwasfi@gmail.com>
2025-07-10 10:24:00 +0300

    add print test

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = FastAPI()
 
 @app.get("/hello")
 def read_root():
+    print("tst")
     return {"message": "This message from the first app, it\'s a get API!"}
-
 
 if __name__ == "__main__":
      uvicorn.run(app, host="127.0.0.1", port=8000)
