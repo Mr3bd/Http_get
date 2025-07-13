5824e5c056b666558d8e823479fc191e21a4a487
Abdullrahman Hawamdeh <abdullrahmanwasfi@gmail.com>
2025-07-13 15:33:39 +0300

    add vuln code.

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
-import subprocess
-import sys
+import sqlite3
+import os
 
 app = FastAPI()
 
-def execute_command(command):
-    subprocess.call(command, shell=True)
-    
+API_SECRET = "supersecretkey123"
+
 @app.get("/hello")
 def read_root():
     print("tst")
-    return {"message": "This message from the first app, it\'s a get API!"}
+    return {"message": "This message from the first app, it's a get API!"}
 
-     
+@app.get("/injection")
+def sql_injection(user: str):
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    query = f"SELECT * FROM users WHERE username = '{user}'"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+    return {"result": result}
 
+@app.post("/exec")
+async def command_injection(request: Request):
+    data = await request.json()
+    cmd = data.get("cmd")
+    os.system(cmd)
+    return {"status": "Command executed"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-    if len(sys.argv) > 1:
-        user_command = sys.argv[1]
-        execute_command(user_command)
-    else:
-        print("Please provide a command.")
