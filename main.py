7f8b75a0a1098614638572394e48dcfd4ac63140
Abdullrahman Hawamdeh <abdullrahmanwasfi@gmail.com>
2025-07-13 15:37:03 +0300

    remove vulnerable code

--- main.py
+++ main.py
@@ -1,33 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 import uvicorn
-import sqlite3
-import os
 
 app = FastAPI()
-
-API_SECRET = "supersecretkey123"
 
 @app.get("/hello")
 def read_root():
     print("tst")
-    return {"message": "This message from the first app, it's a get API!"}
-
-@app.get("/injection")
-def sql_injection(user: str):
-    conn = sqlite3.connect('test.db')
-    cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE username = '{user}'"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    conn.close()
-    return {"result": result}
-
-@app.post("/exec")
-async def command_injection(request: Request):
-    data = await request.json()
-    cmd = data.get("cmd")
-    os.system(cmd)
-    return {"status": "Command executed"}
+    return {"message": "This message from the first app, it\'s a get API!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+     uvicorn.run(app, host="127.0.0.1", port=8000)
