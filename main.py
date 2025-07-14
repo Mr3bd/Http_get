28ba67404b9cc53bc7571d6499f2ae5229d6b527
Abdullrahman Hawamdeh <abdullrahmanwasfi@gmail.com>
2025-07-14 08:29:51 +0300

    Add configmap, and secert files

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
+import os
 
 app = FastAPI()
 
 @app.get("/hello")
 def read_root():
-    print("tst")
-    return {"message": "This message from the first app, it\'s a get API!"}
-
+    secret_value = os.environ.get("MY_SECRET", "Not Set")
+    config_value = os.environ.get("MY_CONFIG", "Not Set")
+    return {"message": f"Hello! Config: {config_value}, Secret: {secret_value}"}
 if __name__ == "__main__":
      uvicorn.run(app, host="127.0.0.1", port=8000)
